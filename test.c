1f63560bc4522238a9b1f5bbc695ef6ee7e488d4
angga saputra <anggalabs@gmail.com>
2024-02-22 19:58:26 +0700

    l

--- test.c
+++ test.c
@@ -1,1 +1,16 @@
-x
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<string> msg {"Hello", "Design Engineering", "AI", "ML", "Data", "and the C++ extension!"};
+
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
+}

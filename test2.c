ce57d7e19728cf1d386f11a04e1071b1598172a3
angga saputra <anggalabs@gmail.com>
2024-02-22 20:11:12 +0700

    uy

--- test2.c
+++ test2.c
@@ -1,6 +1,25 @@
+//code by kurooo
+
 #include <stdio.h>
 
 int main()
 {
-    print ("test");
+    int va = 10;
+    int vb = 5;
+    int result = 0;
+
+    printf("----\n");
+    printf("code by kurooo\n");
+    printf("----\n");
+
+    printf(va);
+    printf(vb);
+
+    result = va+vb;
+
+    printf(result);
+
+    return 0;
 }
+
+// end of file
